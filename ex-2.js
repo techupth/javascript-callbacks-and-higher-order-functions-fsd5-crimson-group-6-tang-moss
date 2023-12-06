d6f2bbd152791dd0965bf69fade37e506169d7e1
Tanawut Wongboot <tanawut.w@outlook.com>
2023-12-06 14:54:51 +0700

    feat(ex-2): find the boolean of array value that meet condition

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,16 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let score of array) {
+    if (operation(score)) {
+      count++;
+      if (count >= 5) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +19,16 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, function (checkScoreCriteria) {
+  return checkScoreCriteria > 70
+});
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, function (checkScoreCriteria) {
+  return checkScoreCriteria > 70
+});
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, function (checkScoreCriteria) {
+  return checkScoreCriteria > 70
+});
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
